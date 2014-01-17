ce87eef469accdea8bbbbc76eeaa22fa3e2a0f52
Ramón Roche <mrpollo@gmail.com>
2014-01-16 16:04:11 -0800

    removes legacy configuration menu view deface this is no longer needed

--- app/overrides/spree_redirects.rb
+++ app/overrides/spree_redirects.rb
@@ -1,9 +1,3 @@
-Deface::Override.new(:virtual_path  => "spree/admin/configurations/index",
-                     :name          => "spree_redirect_config_opt",
-                     :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :partial       => "spree/admin/shared/redirect_config",
-                     :disabled      => false)
-
 Deface::Override.new(:virtual_path  => "spree/admin/shared/_configuration_menu",
                      :name          => "spree_redirect_config_opt_sidebar",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
